d06c54667541b619a27f09be612f18fc274f7149
haily <hailyton00@gmail.com>
2023-03-29 16:15:39 +0700

    ci: implementing husky

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,3 @@
-/* eslint-disable no-unused-vars */
-import _Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return <main className="">Dashboard</main>;
 }
